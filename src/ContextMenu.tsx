6d2227e51e568e6bab14b2e22b4ad666672e4c72
Jeremy Berglund <jtberglund@gmail.com>
2019-05-06 18:29:53 -0700

    Added babel, testing libs, and initial ts files

---
+++ src/ContextMenu.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export interface ContextMenuProps {}
+
+const ContextMenu: React.FunctionComponent<ContextMenuProps> = props => {
+    return <div>{/*  */}</div>;
+};
+
+ContextMenu.defaultProps = {};
+ContextMenu.displayName = "ContextMenu";
+
+export default ContextMenu;
