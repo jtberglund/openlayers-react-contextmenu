2e21d5d226500e3b31129b758410ec3796aa1c89
Jeremy Berglund <jtberglund@gmail.com>
2019-05-17 09:40:25 -0700

    Typecheck openlayers js files + initial context menu implementation

--- src/ContextMenu.tsx
+++ src/ContextMenu.tsx
@@ -1,12 +1,43 @@
-import * as React from "react";
+import * as React from 'react';
 
-export interface ContextMenuProps {}
+import Map from 'ol/PluggableMap';
 
-const ContextMenu: React.FunctionComponent<ContextMenuProps> = props => {
-    return <div>{/*  */}</div>;
+export interface ContextMenuProps {
+    map: Map;
+}
+
+export interface ContextMenuState {
+    isOpen: boolean;
+}
+
+const reducer: React.Reducer<ContextMenuState, any> = (state: ContextMenuState, action: any) => {
+    return state;
 };
 
-ContextMenu.defaultProps = {};
-ContextMenu.displayName = "ContextMenu";
+const ContextMenu: React.FunctionComponent<ContextMenuProps> = ({ children }) => {
+    const [{ isOpen }, dispatch] = React.useReducer(reducer, {
+        isOpen: false
+    });
+    const context = React.useMemo(
+        () => ({
+            isOpen,
+            dispatch
+        }),
+        [isOpen]
+    );
+    return (
+        <ContextMenuContext.Provider value={context}>
+            <div>{children}</div>
+        </ContextMenuContext.Provider>
+    );
+};
+ContextMenu.displayName = 'ContextMenu';
+
+export interface ContextMenuContextState {
+    isOpen: boolean;
+    dispatch: any;
+}
+
+export const ContextMenuContext = React.createContext<ContextMenuContextState | undefined>(undefined);
 
 export default ContextMenu;
